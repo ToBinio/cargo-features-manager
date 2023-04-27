8acad7d7c62d2843df57a207a1e2308af5b9755e
to_binio <tobias.frischmann1@gmail.com>
2023-04-27 10:58:06 +0200

    rename project

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ mod index;
 #[command(name = "cargo")]
 #[command(bin_name = "cargo")]
 enum CargoCli {
-    Feature(FeatureArgs),
+    Features(FeaturesArgs),
 }
 
 #[derive(clap::Args)]
 #[command(author, version, about, long_about = None)]
-struct FeatureArgs {
+struct FeaturesArgs {
     #[arg(long, short)]
     dependency: Option<String>,
 }
 
 fn main() {
-    let CargoCli::Feature(args) = CargoCli::parse();
+    let CargoCli::Features(args) = CargoCli::parse();
 
     if let Err(err) = run(args) {
         execute!(
@@ -39,7 +39,7 @@ fn main() {
     }
 }
 
-fn run(args: FeatureArgs) -> anyhow::Result<()> {
+fn run(args: FeaturesArgs) -> anyhow::Result<()> {
     let mut display = Display::new()?;
 
     if let Some(name) = args.dependency {
